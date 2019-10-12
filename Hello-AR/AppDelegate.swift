bd0d1943d79dc8d51f728376bd1c4625055319c4
Guy Nir <g.nir@f5.com>
2019-10-12 04:57:39 +0300

    AR-Hello

--- Hello-AR/AppDelegate.swift
+++ Hello-AR/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import ARKit
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -35,7 +37,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
+}
 
+extension Double {
+    func deg2Rad() -> Double {
+        return self * .pi / 180.0
+    }
+    
+    func rad2deg() -> Double {
+        return self * 180.0 / .pi
+    }
+}
 
+func +(a: SCNVector3, b: SCNVector3) -> SCNVector3 {
+    return SCNVector3Make(a.x + b.x, a.y + b.y, a.z + b.z)
 }
 
--- Hello-AR/ViewController.swift
+++ Hello-AR/ViewController.swift
@@ -14,20 +14,148 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var add2Button: UIButton!
+    @IBOutlet weak var resetViewButton: UIButton!
+    
+    @IBOutlet weak var DrawButton: UIButton!
+    private let label : UILabel = UILabel()
+    
+    
+    let configuration = ARWorldTrackingConfiguration()
+    
+    var addedcubes : Int = 0
+    
+    
+    
+    //---------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.bringSubviewToFront(addButton)
+        view.bringSubviewToFront(add2Button)
+        view.bringSubviewToFront(resetViewButton)
+        view.bringSubviewToFront(DrawButton)
         
         // Set the view's delegate
         sceneView.delegate = self
         
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints,
+                                       ARSCNDebugOptions.showWorldOrigin]
+        
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
         
+        // Detect planes
+        self.configuration.planeDetection = .horizontal
+        
+        // Run the scene
+        sceneView.session.run(configuration)
+        
+        /* Not sure why
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // Set the scene to the view
         sceneView.scene = scene
+        */
+        
+ 
+        // Let there be light
+        self.sceneView.autoenablesDefaultLighting = true
+        
+        // delegate to self, to enable getting renderer events
+        sceneView.delegate = self
+        
+        // Add Gesture
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(tapped))
+        
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+        
+        //Mark: Start adding elements
+        
+        
+        // Cube
+        let box = SCNBox(width: 0.1, height: 0.2, length: 0.3, chamferRadius: 0.05)
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        box.materials = [material]
+        
+        let boxNode = SCNNode()
+        boxNode.geometry = box
+        boxNode.position = SCNVector3 (1, 0.05, -0.3)
+        
+        
+        
+        
+        
+        
+        // Text
+        let text = SCNText(string: "Maya & Noga", extrusionDepth: 1.0)
+        text.firstMaterial?.diffuse.contents = UIColor.blue
+        
+        let textNode = SCNNode(geometry: text)
+        textNode.position = SCNVector3 (0, 0.05, -0.5)
+        textNode.scale = SCNVector3 (0.02, 0.02, 0.02)
+        
+        self.sceneView.scene.rootNode.addChildNode(textNode)
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
+        
+        //MARK: Sphere - Earth
+        let sphere = SCNSphere(radius: 0.2)
+        sphere.firstMaterial?.diffuse.contents = UIImage(named: "earth.jpg")
+        sphere.firstMaterial?.specular.contents = UIImage(named: "earth_specular_map")
+        
+        
+        let sphereNpde = SCNNode(geometry: sphere)
+        sphereNpde.position = SCNVector3 (-0.5, 0.05, -1.0)
+        let rotateAction = SCNAction.rotateBy(x: 0, y: CGFloat(Double(360).deg2Rad()), z: 0, duration: 10)
+        let rotateForever = SCNAction.repeatForever(rotateAction)
+        sphereNpde.runAction(rotateForever)
+        
+        self.sceneView.scene.rootNode.addChildNode(sphereNpde)
+        
+        print ("Guy Debug")
+        
+        
+        
+        // JellyFish
+        let jellyFishScene = SCNScene(named: "art.scnassets/Jellyfish.scn")
+        let jellyFishNode = jellyFishScene?.rootNode.childNode(withName: "JellyfishSG", recursively: false)
+        jellyFishNode?.position = SCNVector3Make(0.5, -0.2, -0.5)
+        jellyFishNode?.name = "jellyfish"
+        self.sceneView.scene.rootNode.addChildNode(jellyFishNode!)
+        
+    }
+    
+    // Tap action
+    @objc func tapped (recognizer : UIGestureRecognizer) {
+        
+        print ("Tap detected")
+        // Get the scene
+        let sceneView = recognizer.view as! SCNView
+        
+        // Get the location that a touch took place
+        let touchLocation = recognizer.location(in: sceneView)
+        
+        let hitTouch = sceneView.hitTest(touchLocation)
+        
+        
+        // Did we touch any item
+        if hitTouch.isEmpty {
+            print ("found nothing")
+        }
+        else {
+        
+            print ("found a node")
+            
+            
+            let result = hitTouch.first!
+            let geometry = result.node.geometry
+            print (geometry)
+            animateNode(node: result.node)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -35,6 +163,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -47,6 +176,135 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.session.pause()
     }
 
+    
+    //MARK: detect Planes
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+   
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print ("New flat surface detected")
+        
+    }
+    
+    // Detect more area of plane
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        print ("Updating flat surface")
+    }
+    
+    // Understand that 2 surfaces are actually one
+    func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
+        <#code#>
+    }
+    
+    
+    // Set our own surface
+    func createSurface() -> SCNNode {
+        let surfaceNode = SCNNode(geometry: SCNPlane(width: 1, height: 1))
+        surfaceNode.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "wood")
+        surfaceNode.position = SCNVector3Make(0, 0, -1)
+        return surfaceNode
+        
+    }
+    
+    
+    @IBAction func add(_ sender: Any) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        node.position = SCNVector3(0, 0, 0.15 * Double(self.addedcubes))
+        node.eulerAngles = SCNVector3(0, 0, (10.0 * Double(self.addedcubes)).deg2Rad())
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+        self.addedcubes += 1
+        
+    }
+    
+    @IBAction func resetView(_ sender: Any) {
+        self.restartSession()
+    }
+    @IBAction func Add2(_ sender: Any) {
+        
+        // Get Current camera position
+       
+        // Add Sphere
+        let node = SCNNode()
+        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.yellow
+        node.geometry?.firstMaterial?.specular.contents = UIColor.white
+        
+        node.position = SCNVector3(0, 0, 0.15 * Double(self.addedcubes))
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+        self.addedcubes += 1
+        
+    }
+    
+    
+    func restartSession() {
+        self.sceneView.session.pause()
+        self.sceneView.scene.rootNode.enumerateChildNodes { (node, _) in
+            
+            node.removeFromParentNode()
+        }
+        self.sceneView.session.run(self.configuration, options: [.resetTracking, .removeExistingAnchors ])
+        
+        addedcubes = 0
+        
+    }
+    @IBAction func Draw(_ sender: Any) {
+    }
+    
+    
+    
+    func renderer(_ renderer: SCNSceneRenderer, willRenderScene scene: SCNScene, atTime time: TimeInterval) {
+        guard let pointOfView = sceneView.pointOfView else { return }
+        let transform = pointOfView.transform
+        let orientation = SCNVector3(-transform.m31,-transform.m32,-transform.m33)
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let currentPositionOfCamera = orientation + location
+        if (DrawButton.isHighlighted) {
+            print ("location: \(location.x), \(location.y), \(location.z)" )
+            print ("orientation: \(orientation.x), \(orientation.y), \(orientation.z)" )
+            print ("Camera: \(currentPositionOfCamera.x), \(currentPositionOfCamera.y), \(currentPositionOfCamera.z)" )
+            
+            let sphereNode = SCNNode(geometry: SCNSphere(radius: 0.01))
+            sphereNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+            sphereNode.position = currentPositionOfCamera
+            self.sceneView.scene.rootNode.addChildNode(sphereNode)
+        }
+        
+    }
+    
+    func animateNode(node: SCNNode) {
+        // Make sure only 1 animation is taking place at a time
+        if (node.animationKeys.isEmpty) {
+
+            // Make sure we animate inside a transaction
+            SCNTransaction.begin()
+            
+            // Set the animation
+            let spin = CABasicAnimation(keyPath: "position")
+            let currPos = node.presentation.position
+            spin.fromValue = currPos
+            spin.toValue = SCNVector3Make(currPos.x, currPos.y, currPos.z - 0.1)
+            spin.autoreverses = true
+            spin.duration = 0.5
+            spin.repeatCount = 5
+            node.addAnimation(spin, forKey: "position")
+            
+            // Set action at end of animation
+            SCNTransaction.completionBlock = {
+                if node.name == "jellyfish" {
+                    node.removeFromParentNode()
+                }
+            }
+            
+            // End of transaction
+            SCNTransaction.commit()
+        }
+    }
+    
     // MARK: - ARSCNViewDelegate
     
 /*
@@ -71,5 +329,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
+    }
+}
+
+
+extension CGFloat {
+    static func random() -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UInt32.max)
+    }
+}
+
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(red:   .random(),
+                       green: .random(),
+                       blue:  .random(),
+                       alpha: 1.0)
     }
 }
